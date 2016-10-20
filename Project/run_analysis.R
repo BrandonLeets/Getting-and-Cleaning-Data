dad7903167b257dd10ad78f27b09aa434a51742f
Brandon <BigRigLeets@Gmail.com>
2016-10-19 22:36:30 -0700

    Fixing the assignment

--- Project/run_analysis.R
+++ Project/run_analysis.R
@@ -1,77 +1,51 @@
 run_analysis <- function(){
-  library(data.table)
-  library(knitr)
-  path <- getwd()
+  library(dplyr)
+
   #get the data
   url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   fileName <- "Dataset.zip"
-  if(!file.exists(path)){dir.create(path)}
-  download.file(url, file.path(path, fileName))
+  download.file(url, fileName)
   #unzip the files
-  unzip(file.path(path, fileName))
-  pathIn <- file.path(path, "UCI HAR Dataset")
-  list.files(pathIn, recursive = TRUE)
-  #Read in files
-  dtsubtrain <- fread(file.path(pathIn, "train","subject_train.txt"))
-  dtsubtest <- fread(file.path(pathIn, "test","subject_test.txt"))
-  dtacttrain <- fread(file.path(pathIn, "train", "Y_train.txt"))
-  dtacttest <- fread(file.path(pathIn, "test", "Y_test.txt"))
-  fileToMakeTable <- function(path){
-    df <- read.table(path)
-    dt <- data.table(df)
-  }
-  dttrain <- fileToMakeTable(file.path(pathIn, "train", "X_train.txt"))
-  dttest <- fileToMakeTable(file.path(pathIn, "test", "X_test.txt")) 
-  #begin merging the datasets
-  dtsub <- rbind(dtsubtrain, dtsubtest)
-  setnames(dtsub, "V1", "subject")
-  dtact <- rbind(dtacttrain, dtacttest)
-  setnames(dtact, "V1", "activityNumber")
+  unzip(fileName)
+  setwd("./UCI HAR Dataset")
+  #read in the features file
+  feat <- read.table("features.txt", colClasses = "character")
+  #read in the activity labels
+  actlabels <- read.table("activity_labels.txt", colClasses = "character")
+  #Read training files
+  dttraindata <- read.table("./train/X_train.txt")
+  dttrainsubject <- read.table("./train/subject_train.txt")
+  dttrainactivity <- read.table("./train/y_train.txt")
+  #combine all together
+  dttrain <- cbind(dttrainsubject, dttrainactivity, dttraindata)
+  #change the column nmaes train
+  colnames(dttrain) <- c("subject", "activity", feat[,2])
+  dttrain$condition <- "train"
+  #Read in testing files
+  dttestdata <- read.table("./test/X_test.txt")
+  dttestsubject <- read.table("./test/subject_test.txt")
+  dttestactivity <- read.table("./test/y_test.txt")
+  #combine all together
+  dttest <- cbind(dttestsubject, dttestactivity, dttestdata)
+  #Change the column names test 
+  colnames(dttest) <- c("subject", "activity", feat[,2])
+  dttest$condition <- "test"
+
+  
+  #merge both data sets
   dt <- rbind(dttrain, dttest)
-  #merge the columns
-  dtsub <- cbind(dtsub, dtact)
-  dt <- cbind(dtsub, dt)
-  #set the key
-  setkey(dt, subject, activityNumber)
-  #extract the mean and stdev
-  dtfeat <- fread(file.path(pathIn, "features.txt"))
-  setnames(dtfeat, names(dtfeat), c("featureNum", "featureName"))
-  dtfeat <- dtfeat[grepl("mean\\(\\)|std\\(\\)", featureName)]
-  dtfeat$featureCode <- dtfeat[,paste0("V", featureNum)]
-  head(dtfeat)
-  select <- c(key(dt), dtfeat$featureCode)
-  dt <- dt[,select, with=FALSE]
-  #use descriptive activity names
-  dtActnames <- fread(file.path(pathIn, "activity_labels.txt"))
-  setnames(dtActnames, names(dtActnames), c("activityNumber", "activityName"))
-  #label with descrititve anmes
-  dt <- merge(dt, dtActnames, by = "activityNumber", all.x = TRUE)
-  setkey(dt, subject, activityNumber, activityName)
-  dt <- data.table(melt(dt, key(dt), variable.name = "featureCode"))
-  dt <- merge(dt, dtfeat[,list(featureNum, featureCode, featureName)], by = "featureCode", all.x = TRUE)
-  dt$activity <- factor(dt$activityName)
-  dt$feature <- factor(dt$featureName)
-  grepthis <- function(name){
-    grepl(name, dt$feature)
-  }
-  n <- 2
-  y <- matrix(seq(1,n), nrow = n)
-  x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol = nrow(y))
-  dt$featDomain <- factor( x %*% y, labels = c("Time", "Feq"))
-  x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol = nrow(y))
-  dt$featInstrument <- factor(x %*% y, labels = c("Accelerometer","Gyroscope"))
-  x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol = nrow(y))
-  dt$featAcceleration <- factor(x %*% y, labels = c(NA, "Body", "Gravity"))
-  x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol = nrow(y))
-  dt$featVariable <- factor( x %*% y, labels = c("Mean","STDEV"))
-  dt$featJerk <- factor(grepthis("Jerk"), labels = c(NA, "Jerk"))
-  dt$featMagnitude <- factor(grepthis("Mag"), labels = c(NA, "Magnitude"))
-  n <- 3
-  y <- matrix(seq(1,n), nrow=n)
-  x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol=nrow(y))
-  dt$featAxis <- factor(x %*% y, labels = c(NA,"X","Y","Z"))
-  setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, featJerk, featMagnitude, featVariable, featAxis)
-  dtTidy <- dt[, list(count = .N, average = mean(value)), by = key(dt)]
-  f <- file.path(path, "DatasetHumanActivityRecoginitionUsingSmartphones.txt")
-  write.table(dtTidy, f, quote = FALSE, sep = "\t", row.names = FALSE)
+  dt <- dt[,c(ncol(dt), 2:ncol(dt)-1)]
+  
+  #rename the activities
+  dt$activity <- as.factor(dt$activity)
+  levels(dt$activity) <- actlabels$V2
+  
+  #create summary
+  actsummary <- group_by(dt,condition, subject, activity)
+  actsummary <- summarise_each(actsummary, funs(mean))
+  
+  #save the files
+  write.table(dt, "tidyData.txt", row.names = FALSE)
+  write.table(actsummary, "tidydatasummary.txt", row.names = FALSE)
+  
 }
